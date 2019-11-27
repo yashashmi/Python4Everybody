54cd1f31685ffd53b13fc38deff8a76b1470beda
Hashmi, Yasub <yas.hashmi@gmail.com>
2019-11-27 14:07:21 +0530

    Upto geodata

--- _Trial.py
+++ _Trial.py
@@ -1,12 +1,15 @@
-import re
+# import re
 
-text = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
-domain = re.findall('@(\S+)', text)
-print (domain[0])
+# text = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+# domain = re.findall('@(\S+)', text)
+# print (domain[0])
 
-email = re.findall( '\S+?@\S+', text)
-print(email)
+# email = re.findall( '\S+?@\S+', text)
+# print(email)
 
-x = 'From: Using the : character'
-y = re.findall('^F.+:', x)
-print(y)
+# x = 'From: Using the : character'
+# y = re.findall('^F.+:', x)
+# print(y)
+
+print (ord('G'))
+print (chr(108), chr(105), chr(115), chr(116))
---
+++ emaildb.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+loopCount = 0
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    org = pieces[1].split('@')[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    if(loopCount %10 == 0):
+        conn.commit()
+    
+    loopCount += 1
+conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
